d97919107c5e5313e72563bb05b4168cd1c3bb80
ckyst <ckyst@10.50.46.225>
2024-11-28 03:04:40 +0900

    update

--- Individual/project/src/project/main.java
+++
@@ -1,8 +0,0 @@
-package project;
-
-public class main {
-    public static void main(String[] args) {
-        NonogramMonitor nonograms = new NonogramMonitor();
-        nonograms.show();
-    }
-}
